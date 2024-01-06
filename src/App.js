c224751ac42770cc3130d43455ce13927424346d
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-01-06 12:47:14 +0800

    removed pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 import ClientProfile from "./Components/ClientProfile/ClientProfile";
-import TutorProfile from "./Components/TutorProfile/TutorProfile";
-import Navbar from "./Components/Navbar/Navbar";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -9,11 +7,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Navbar /> */}
         <Routes>
           <Route path="/PremiumTutors/" element={<ClientProfile />} />
-          <Route path="/PremiumTutors/tutor" element={<TutorProfile />} />
-          {/* <Route path="/PremiumTutors/gpt" element={<Chatgpt />} /> */}
         </Routes>
       </div>
     </Router>
